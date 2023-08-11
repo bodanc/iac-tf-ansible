fa1850611dbbfc8a04da70059498ee8470725c72
Bogdan Dancu <bogdan@domain.local>
2023-08-11 19:19:39 +0200

    tf fmt on backend.tf

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
 terraform {
   required_version = ">=1.0.0"
   backend "s3" {
-    region = "us-east-1"
+    region  = "us-east-1"
     profile = "default"
-    key = "tf-state-file"
-    bucket = "tf-state-235"
+    key     = "tf-state-file"
+    bucket  = "tf-state-235"
   }
 }
